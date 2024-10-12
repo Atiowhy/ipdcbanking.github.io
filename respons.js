ba4ad32f7e246f022584d6c909477a876d7997c4
Atiowhy <atiowahyudi02@gmail.com>
2024-10-12 15:49:15 +0700

    slicing done

---
+++ respons.js
@@ -0,0 +1,18 @@
+// // Tambahkan kode ini pada file respons.js Anda
+
+// // Fungsi untuk menampilkan dan menyembunyikan menu hamburger
+// function toggleMenu() {
+//   const hamburger = document.querySelector(".hamburger");
+//   const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+//   if (hamburgerMenu.style.display === "block") {
+//     hamburgerMenu.style.display = "none";
+//     hamburger.classList.remove("active");
+//   } else {
+//     hamburgerMenu.style.display = "block";
+//     hamburger.classList.add("active");
+//   }
+// }
+
+// // Tambahkan event listener pada hamburger
+// document.querySelector(".hamburger").addEventListener("click", toggleMenu);
